0a91a8fa4b00be72539ce7fd356a0a485b6f176c
Jyoti Kumari <jyotikumari01310@gmail.com>
2026-07-10 20:19:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (91.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,64 +1,32 @@
 class Solution {
+
     public int search(int[] nums, int target) {
 
-        int pivote=peakele(nums);
-        if(pivote==-1){
-            return binarySearch(nums,target,0,nums.length-1);
-        }
-        if(nums[pivote]==target){
-            return pivote;
-        }
-        if(target>=nums[0]){
-            return binarySearch(nums,target,0,pivote-1);
-        }
-        return binarySearch(nums,target,pivote+1,nums.length-1);
-    }
+        int left = 0, right = nums.length - 1;
 
-    public int binarySearch(int [] arr,int target, int start, int end){
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(target< arr[mid]){
-                end=mid-1;
-            }
-            else if(target>arr[mid]){
-                 start=mid+1;
-            }
-            else{
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
                 return mid;
+
+            if (nums[left] <= nums[mid]) {
+
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else {
+
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
         }
+
         return -1;
     }
-
-
-    int peakele(int [] arr){
-
-        int start =0;
-        int end = arr.length-1;
-
-        while(start<=end){
-            
-            int mid= start + (end-start)/2;
-
-            if(mid < end && arr[mid] > arr[mid+1]){
-                return mid;
-            
-            }
-            if(mid>start && arr[mid] < arr[mid-1]){
-                return mid-1;
-            }
-            if (arr[mid]<=arr[start]){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-
-            }
-        }
-        return -1;
-    }
-
-
-        
-    
 }
